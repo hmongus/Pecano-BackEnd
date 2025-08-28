d75c402fb82aad3ab3f7863c46dfb1b424e9f791
Usuario <jose1506.jlmv23@gmail.com>
2025-08-28 13:53:03 -0500

    batman

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,29 @@
 INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
-VALUES ('02266294', 7, 10, 1, 'OBREROS');
+VALUES ('02266776', 6, 9, 2, 'GERENTES');
 
 INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
-VALUES ('02266617', 7, 11, 3, 'SUPERVISORES');
+VALUES ('02266833', 7, 26, 4, 'OBREROS');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02266889', 8, 16, 4, 'SUPERVISORES');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02267005', 6, 8, 4, 'GERENTES');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02272136', 8, 16, 2, 'OBREROS');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02274043', 7, 9, 5, 'SUPERVISORES');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02274681', 8, 11, 0, 'GERENTES');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02275410', 8, 9, 1, 'OBREROS');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02276155', 7, 14, 2, 'SUPERVISORES');
+
+INSERT INTO employee (dni, horas_laboradas, dias_laborados, faltas, tipo_trabajador)
+VALUES ('02276542', 9, 20, 3, 'GERENTES');
